9c38a363ec33b12fd87047285c2c252390833250
Saptarshi Mallick <sapmallick@gmail.com>
2021-09-20 17:32:43 -0700

    Initial project files

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       saptarshimallick                                          */
+/*    Created:      Mon Sep 20 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() 
+{
+  
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
